6c93bbc885a71dd133ae50a36739ca8c816327c2
Jamie Gerrard Lievesley <jamie@jamiegl.co.uk>
2024-11-10 23:38:23 +0000

    Switch from tsup to parcel

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig(() => ({
-  shims: true,
-  cjsInterop: true,
-  dts: true,
-  sourcemap: true,
-  entry: ["./src/index.ts"],
-}));
