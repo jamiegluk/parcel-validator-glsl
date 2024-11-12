205346400215d1edba5af9f9df1fa4fa135c2132
Jamie Gerrard Lievesley <jamie@jamiegl.co.uk>
2024-11-12 00:35:59 +0000

    Reorganize code

---
+++ src/GLSLValidator.ts
@@ -0,0 +1,143 @@
+import ThrowableDiagnostic, { escapeMarkdown } from "@parcel/diagnostic";
+import { Validator } from "@parcel/plugin";
+// @ts-ignore -- Parcel can't seem to recognize the typings for this, but Typescript does
+import { loadConfig } from "@parcel/utils";
+import { exec } from "child_process";
+import { createRequire } from "module";
+import { Config, DEFAULT_CONFIG, validateConfig } from "./config";
+import { parseValidateResult } from "./parseValidateResult";
+import { augmentCodeToFile } from "./augmentCodeToFile";
+
+// Workaround https://github.com/parcel-bundler/parcel/issues/6925#issuecomment-1003935487
+const req = createRequire(__dirname);
+
+/**
+ * Parcel optimizer plugin to remove the #version directive from GLSL files.
+ */
+export const GLSLValidator = new Validator({
+  async getConfig({ asset, resolveConfig, options }) {
+    const configNames = [
+      "parcel-validator-glsl.config.json",
+      "parcel-validator-glsl.config.js",
+    ];
+    const filePath = await resolveConfig(configNames);
+    if (filePath) {
+      // Observe changes to the config
+      // @ts-expect-error -- type is missing, but function exists
+      asset.invalidateOnFileChange(filePath);
+
+      // For whatever reason, the validator doesn't rerun when the file changes
+      // So manually request such
+      // @ts-expect-error -- type is missing, but function exists
+      asset.invalidateOnFileChange(asset.filePath);
+      // Nor does it rerun when the build is restarted
+      // @ts-expect-error -- type is missing, but function exists
+      asset.invalidateOnStartup();
+    }
+
+    // Load and validate the config
+    const loadedConfig = await loadConfig(
+      asset.fs,
+      asset.filePath,
+      configNames,
+      options.projectRoot,
+    );
+    const config: Required<Config> = {
+      ...DEFAULT_CONFIG,
+      ...(loadedConfig?.config ?? {}),
+    };
+    validateConfig(config);
+    return config;
+  },
+
+  async validate({ asset, logger, config: _config, options }) {
+    const config = _config as Readonly<Required<Config>>;
+
+    // Get OS independent path to validator
+    let validator = "glslang-validator-prebuilt-predownloaded/bin/";
+    switch (process.platform) {
+      case "darwin":
+        validator += "glslangValidator.darwin";
+        break;
+      case "win32":
+        validator += "glslangValidator.exe";
+        break;
+      case "linux":
+        validator += "glslangValidator.linux";
+        break;
+      default:
+        throw new ThrowableDiagnostic({
+          diagnostic: {
+            message: "Platform not supported: " + process.platform,
+          },
+        });
+    }
+
+    // Resolve path to validator
+    validator = req.resolve(validator);
+
+    // TODO append to code
+    const code = await asset.getCode();
+    const [filePath, lineOffset, isTmpFile] = await augmentCodeToFile(
+      asset,
+      config,
+      options,
+      code,
+    );
+
+    // Add standard arguments and options
+    const cmd = `"${validator}" -l -DVALIDATE ${config.commandArguments} "${filePath}"`;
+
+    // Run the glslValidator command and handle the output
+    return new Promise((resolve) => {
+      exec(cmd, async (error, stdout, stderr) => {
+        // Clean up temp file
+        if (isTmpFile) {
+          try {
+            await asset.fs.rimraf(filePath);
+          } catch {}
+        }
+
+        let message: string;
+        if (error) {
+          // Validation failed
+          message =
+            stdout || stderr
+              ? ["GLSL validation failed", stdout, stderr].join("\n")
+              : error.message || "Unknown error";
+        } else {
+          // Validation passed
+          // There may still be warnings
+          message = ["GLSL validation passed, with warnings", stdout, stderr]
+            .filter(Boolean)
+            .join("\n");
+        }
+        // Parse errors and warnings
+        const result = parseValidateResult(
+          message,
+          asset,
+          code,
+          filePath,
+          lineOffset,
+        );
+
+        // Handle any output in the case no errors or warnings could be parsed
+        if (result.errors.length === 0 && result.warnings.length === 0) {
+          if (stdout && stdout.trim() !== filePath) {
+            logger.log({
+              message: escapeMarkdown(stdout),
+            });
+          }
+          if (stderr) {
+            logger.error({
+              message: escapeMarkdown(stderr),
+            });
+          }
+        }
+
+        // Forward the result
+        resolve(result);
+      });
+    });
+  },
+});
--- src/parseValidateResult.ts
+++ src/parseValidateResult.ts
@@ -10,58 +10,42 @@ import { name } from "~/package.json";
 
 /**
  * Parses the output of the `gslangValidator` command into
- * a list of Parcel validator {@link DiagnosticCodeHighlight}s for just warnings *or* errors.
- * @param createRegex Function creating a regular expression to match the issues (warnings *or* errors) in the output.
- * @param mainMessage Output of `gslangValidator` command (`stdout` and `stderr` joined).
+ * a Parcel validator {@link ValidateResult}.
+ * @param message Output of `gslangValidator` command (`stdout` and `stderr` joined).
+ * @param asset Parcel asset.
+ * @param code Original asset code, without additional code injected.
+ * @param filePath File path of the temporary modified asset, or the original asset if no code was injected.
  * @param lineOffset Count of lines that were injected into the code.
- * @returns Tuple of:
- *   1. Parcel validator {@link DiagnosticCodeHighlight} with parsed issues (warnings *or* errors).
- *   2. Parsed message, with above highlighted code issues removed.
+ * @returns Parcel validator {@link ValidateResult} with parsed message, errors and warnings.
  */
-function parseCodeHighlightsByRegex(
-  createRegex: () => RegExp,
-  mainMessage: string,
+export function parseValidateResult(
+  message: string,
+  asset: Asset,
+  code: string,
+  filePath: string,
   lineOffset: number,
-): [codeHighlights: DiagnosticCodeHighlight[], message: string] {
-  // Match regex
-  const matches = Array.from(mainMessage.matchAll(createRegex()));
-
-  // Get issues from regex match groups
-  let issues: { line: number; message: string }[] = matches.map(
-    ([, , line, message]) => ({
-      line: parseInt(line!, 10) - lineOffset,
-      message: message || "Unknown issue",
-    }),
+): ValidateResult {
+  // Return parsed message, errors and warnings
+  const warning = parseDiagnosticByRegex(
+    () => /^WARNING: (.+):(\d+): (.+)$/gm,
+    message,
+    asset,
+    code,
+    filePath,
+    lineOffset,
   );
-
-  // Merge issues that are on the same line
-  issues = issues.reduce(
-    (issues, { line, message }) => {
-      if (issues.length > 0) {
-        const prevEntry = issues[issues.length - 1]!;
-        if (prevEntry.line === line) {
-          prevEntry.message += "; " + message;
-          return issues;
-        }
-      }
-      issues.push({ line, message });
-      return issues;
-    },
-    [] as typeof issues,
+  const error = parseDiagnosticByRegex(
+    () => /^ERROR: (.+):(\d+): (.+)$/gm,
+    message,
+    asset,
+    code,
+    filePath,
+    lineOffset,
   );
-
-  // Convert to Parcel code highlights
-  const codeHighlights: DiagnosticCodeHighlight[] = issues.map(
-    ({ line, message }) => {
-      const start = { line, column: 0 };
-      return { start, end: start, message: escapeMarkdown(message) };
-    },
-  );
-
-  // Strip errors out of message, they will be rendered separately
-  mainMessage = mainMessage.replaceAll(createRegex(), "");
-
-  return [codeHighlights, mainMessage];
+  return {
+    warnings: warning ? [warning] : [],
+    errors: error ? [error] : [],
+  };
 }
 
 /**
@@ -127,40 +111,56 @@ function parseDiagnosticByRegex(
 
 /**
  * Parses the output of the `gslangValidator` command into
- * a Parcel validator {@link ValidateResult}.
- * @param message Output of `gslangValidator` command (`stdout` and `stderr` joined).
- * @param asset Parcel asset.
- * @param code Original asset code, without additional code injected.
- * @param filePath File path of the temporary modified asset, or the original asset if no code was injected.
+ * a list of Parcel validator {@link DiagnosticCodeHighlight}s for just warnings *or* errors.
+ * @param createRegex Function creating a regular expression to match the issues (warnings *or* errors) in the output.
+ * @param mainMessage Output of `gslangValidator` command (`stdout` and `stderr` joined).
  * @param lineOffset Count of lines that were injected into the code.
- * @returns Parcel validator {@link ValidateResult} with parsed message, errors and warnings.
+ * @returns Tuple of:
+ *   1. Parcel validator {@link DiagnosticCodeHighlight} with parsed issues (warnings *or* errors).
+ *   2. Parsed message, with above highlighted code issues removed.
  */
-export function parseValidateResult(
-  message: string,
-  asset: Asset,
-  code: string,
-  filePath: string,
+function parseCodeHighlightsByRegex(
+  createRegex: () => RegExp,
+  mainMessage: string,
   lineOffset: number,
-): ValidateResult {
-  // Return parsed message, errors and warnings
-  const warning = parseDiagnosticByRegex(
-    () => /^WARNING: (.+):(\d+): (.+)$/gm,
-    message,
-    asset,
-    code,
-    filePath,
-    lineOffset,
+): [codeHighlights: DiagnosticCodeHighlight[], message: string] {
+  // Match regex
+  const matches = Array.from(mainMessage.matchAll(createRegex()));
+
+  // Get issues from regex match groups
+  let issues: { line: number; message: string }[] = matches.map(
+    ([, , line, message]) => ({
+      line: parseInt(line!, 10) - lineOffset,
+      message: message || "Unknown issue",
+    }),
   );
-  const error = parseDiagnosticByRegex(
-    () => /^ERROR: (.+):(\d+): (.+)$/gm,
-    message,
-    asset,
-    code,
-    filePath,
-    lineOffset,
+
+  // Merge issues that are on the same line
+  issues = issues.reduce(
+    (issues, { line, message }) => {
+      if (issues.length > 0) {
+        const prevEntry = issues[issues.length - 1]!;
+        if (prevEntry.line === line) {
+          prevEntry.message += "; " + message;
+          return issues;
+        }
+      }
+      issues.push({ line, message });
+      return issues;
+    },
+    [] as typeof issues,
   );
-  return {
-    warnings: warning ? [warning] : [],
-    errors: error ? [error] : [],
-  };
+
+  // Convert to Parcel code highlights
+  const codeHighlights: DiagnosticCodeHighlight[] = issues.map(
+    ({ line, message }) => {
+      const start = { line, column: 0 };
+      return { start, end: start, message: escapeMarkdown(message) };
+    },
+  );
+
+  // Strip errors out of message, they will be rendered separately
+  mainMessage = mainMessage.replaceAll(createRegex(), "");
+
+  return [codeHighlights, mainMessage];
 }
